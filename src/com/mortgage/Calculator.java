c778b9998dac625259c1cb20a569e7bd48c3f8d3
SamuelOthieno <douglasothieno@gmail.com>
2025-11-29 19:50:32 +0300

    Add logic

--- src/com/mortgage/Calculator.java
+++ src/com/mortgage/Calculator.java
@@ -1,103 +1,39 @@
 package com.mortgage;
 
-import java.io.IOException;
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Calculator {
-    static boolean isCompany = false;
-    static short dateOfBirth;
-    static short currentYear;
-    static byte age;
-    static String name;
-    static String address;
-    static final byte percent = 100;
-    static final byte monthsInAYear = 12;
-    static float monthlyInterest;
-    static float annualInterest;
-    static int numberOfPayment;
-    static double mortgage;
-    static int principal;
-    static String helloWorld = " welcome to your most trusted Mortgage Calculator.";
-    static String getStarted = "Lets get started, shall we?";
-    static byte years;
-    static String mortgageCalculated;
 
+    public static void main(String[] args) {
 
+        Methods methods = new Methods();
 
+        methods.userName();
 
-    static void main(String[] args) {
+        while (true) {
+            methods.principal();
+            methods.mortgagePeriod();
+            methods.annualInterest();
+            methods.mortgageCalculated();
 
-        Scanner scanner = new Scanner(System.in);
+            while (true){
 
-        while (true){
-            try {
+                System.out.println("\n Would you like to calcaulate another mortgage?)");
+                System.out.print("(Yes/No): ");
+                String choice = methods.scanner.nextLine().trim().toLowerCase();
 
-                System.out.print("Enter name: ");
-                name =  scanner.nextLine().toUpperCase().trim();
+                switch (choice) {
+                    case "yes":
+                        break;
 
-                if (name.isEmpty()){
-                    throw new IllegalArgumentException("Enter your name");
+                    case "no":
+                        System.out.println("Thank you for using the Mortgage Calculator. Goodbye!");
+                        methods.scanner.close();
+                        return;
+
+                    default:
+                        System.out.println("Invalid choice. Please type Yes or No.");
                 }
                 break;
-                
-            }catch (Exception e){
-                System.out.println("Something went wrong, please try again");
             }
         }
-
-        System.out.println("Hello " + name + helloWorld);
-        System.out.println(getStarted);
-
-        while (true){
-            try {
-                System.out.print("Enter amount: ");
-                principal = scanner.nextInt();
-                scanner.nextLine();
-
-                if (principal >= 10_000_000 && principal <= 1_000_000_000)
-                    break;
-
-            }catch (Exception e){
-                System.out.println("Something went wrong.");
-
-            }finally {
-                System.out.println("Please try again");
-            }
-        }
-
-        while (true){
-            System.out.print("Enter period(Years): ");
-            years = scanner.nextByte();
-            scanner.nextLine();
-
-            if (years >= 1 && years <= 60){
-                numberOfPayment = years * monthsInAYear;
-                break;
-            }
-            System.out.print("Enter value between 1 to 60 years");
-        }
-
-        while (true){
-            System.out.print("Enter Annual Interest: ");
-            annualInterest = scanner.nextFloat();
-            scanner.nextLine();
-
-            if (annualInterest >=1 && annualInterest <= 30){
-                monthlyInterest = annualInterest / percent / monthsInAYear;
-                break;
-            }
-            System.out.print("Enter a value between 1% to 30% ");
-        }
-
-        mortgage = principal
-                * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayment)
-                /Math.pow(1 + monthlyInterest, numberOfPayment) - 1;
-
-        mortgageCalculated = NumberFormat.getCurrencyInstance().format(mortgage);
-
-        System.out.println(mortgageCalculated);
-
-        scanner.close();
     }
 }
